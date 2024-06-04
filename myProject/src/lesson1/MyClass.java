b4b57a8403e9734e58081cc8a6c7999962fa7639
e19166 <e19166@eng.pdn.ac.lk>
2024-06-04 23:45:23 +0530

    lesson1 package started

---
+++ myProject/src/lesson1/MyClass.java
@@ -0,0 +1,14 @@
+package lesson1;
+
+public class MyClass {
+    /*
+    This is my first program
+     */
+    public static void main(String[] args) {
+        int x = 10;
+        float my_decimal = (float)4.5;
+
+        System.out.println(x);
+        System.out.println(my_decimal);
+    }
+}
